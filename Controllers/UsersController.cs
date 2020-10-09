78e416f80c1bf8546155cb33f2701c12037d308f
Ania Vakht <annavakht@gmail.com>
2020-10-09 12:54:24 +0300

    Add project files.

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CRUDtoFIle.Models;
+
+namespace CRUDtoFIle.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        UserRepository repo;
+
+        public UsersController(UserRepository repo)
+        {
+            this.repo = repo;
+            if (!repo.users.Any())
+            {
+                repo.users.Add(new User { Email = "tom26@gmail.com", Name = "Tom", Age = 26 });
+                repo.users.Add(new User { Email = "alice31@gmail.com", Name = "Alice", Age = 31 });
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> Get()
+        {            
+            return repo.users.ToList();
+        }
+
+    }
+}
---
+++ Models/UserRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDtoFIle.Models
+{
+    public class UserRepository
+    {
+        public List<User> users { get; set; }
+        public UserRepository()
+        {
+            users = new List<User>();
+        }
+
+        public void add(User user) 
+        {
+            users.Add(user);
+        }
+
+        public void update(User user) 
+        {
+            
+        }
+
+        public User get(string email) {
+            return null;
+        }
+
+        public List<User> getAll()
+        {
+            return users;
+        }
+
+        public void delete(string email) 
+        {
+            for (int i = 0; i < users.Count; i++)
+            {
+                if (users[i].Email.Equals(email))
+                {
+                    users.RemoveAt(i);
+                }
+            }
+        }
+
+    }
+}
